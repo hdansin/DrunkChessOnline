fde33390b73eeb86401efe9cd7010dee1147f517
hdansin <hdansin@mailbox.org>
2021-02-25 10:41:38 -0500

    Implemented socket io and got it sending moves/switched back and forth. Core functionality works

--- index.js
+++ index.js
@@ -3,6 +3,18 @@ const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
+var numRooms = 0;
+var openRooms = []; // An array of open rooms
+
+var roomIsOpen = function (room, roomArr) {
+  for (let i = 0; i < roomArr.length; i++) {
+    if (roomArr == room) {
+      return true;
+    }
+  }
+  return false;
+}
+
 app.use('/assets', express.static(__dirname + '/assets'));
 
 app.get('/', (req, res) => {
@@ -13,12 +25,45 @@ io.on('connection', (socket) => {
   console.log('a user connected');
   socket.on('disconnect', () => {
     console.log('user disconnected');
-    console.log(socket.id);
   });
 
-  socket.on('username submitted', (uname) => {
-    console.log('user ' + uname + ' wants to create a game');  
+  socket.on('game-created', () => {
+    numRooms++;
+    let room = "" + numRooms;
+    openRooms.push(room);
+    socket.join(room);
+    console.log(socket.rooms);
+    io.to(room).emit('creating-room', room);
   });
+
+  socket.on('roomID-submitted', (roomID, playerID) => {
+    if (roomIsOpen(roomID, openRooms)) {
+      // Join the room
+      socket.join(roomID);
+      io.to(roomID).emit('player2-joining');
+
+      // Remove the room from openRooms
+      for (let i = 0; i < openRooms.length; i++) {
+        if (openRooms[i] == roomID) {
+          openRooms.splice(i, 1);
+          i = openRooms.length;
+        }
+      }
+    } else {
+      io.to(playerID).emit('room-full');
+    }
+  });
+
+  socket.on('move', (moveObj, roomID) => {
+    console.log(moveObj + ' in ' + roomID);
+    socket.to(roomID).emit('opponent-move', moveObj, roomID);
+  });
+
+  socket.on('switch', (c1, c2, fen, roomID) => {
+    console.log("switching pieces " + c1 + " with " + c2);
+    socket.to(roomID).emit('switch-pieces', c1, c2, fen);
+  });
+
 });
 
 http.listen(3000, () => {
